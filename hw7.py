284fb68917c862b94db5502711235bb0b37165b2
spetrov1 <svetoslav.petrov@lights.digital>
2020-12-30 23:56:17 +0200

    Add base code with plotting some points

---
+++ hw7.py
@@ -0,0 +1,19 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# read_data(filePath) -> List[Points]
+
+# random_init_centroids(data, kCentroids) -> list of k points
+
+# kMeans(k, data) -> List[Clusters]
+
+N = 5
+x = np.random.rand(N)
+y = np.random.rand(N)
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+
+plt.scatter(x, y)
+
+plt.show()
